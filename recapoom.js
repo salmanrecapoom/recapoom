18275704730c302a84be718b10436a73e693e1de
Salman Lari <salman,recapoom@gmail.com>
2016-03-18 05:40:49 +0300

    Moved 'fortune cokkie' functionality into module.

--- recapoom.js
+++ recapoom.js
@@ -2,13 +2,14 @@ var express = require('express');
 
 var app = express();
 
-var fortunes = [
+var fortune = require('./lib/fortune.js')
+/*var fortunes = [
 		"Conquer your fears or they will conquer you.",
 		"Rivers need springs.",
 		"Do not fear what you don't know.",
 		"You will have a pleasant surprise.",
 		"Whenever possible, keep it simple.",
-];
+]; */
 
 // set up handlebars view engine
 var handlebars = require('express-handlebars')
@@ -25,9 +26,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/about', function(req, res){
-	var randomFortune =
-			fortunes[Math.floor(Math.random() * fortunes.length)];
-	res.render('about', { fortune: randomFortune });
+	res.render('about', { fortune: fortune.getFortune() });
 });
 
 // 404 catch-all handler (middleware)
